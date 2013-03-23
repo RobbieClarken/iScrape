b9df7b2174edeb774ca973f7b0a94d20dc70721f
RobbieClarken <robbie.clarken@gmail.com>
2013-03-23 23:20:23 +1100

    Initial Commit

---
+++ iScrape/ASViewController.m
@@ -0,0 +1,29 @@
+//
+//  ASViewController.m
+//  iScrape
+//
+//  Created by Robbie Clarken on 23/03/13.
+//  Copyright (c) 2013 Robbie Clarken. All rights reserved.
+//
+
+#import "ASViewController.h"
+
+@interface ASViewController ()
+
+@end
+
+@implementation ASViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iScrape/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iScrape
+//
+//  Created by Robbie Clarken on 23/03/13.
+//  Copyright (c) 2013 Robbie Clarken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
